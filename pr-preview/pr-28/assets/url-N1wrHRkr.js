bd7a4f0f060b8a1e95265761a8440fedb4155c0b
davidirvine <davidirvine@users.noreply.github.com>
2026-04-29 05:55:14 +0000

    Remove preview for PR 28 🛬

--- pr-preview/pr-28/assets/url-N1wrHRkr.js
+++
@@ -1,8 +0,0 @@
-//#region src/lib/stubs/url.js
-function pathToFileURL(path) {
-	return new URL("file://" + path);
-}
-var URL = globalThis.URL;
-globalThis.URLSearchParams;
-//#endregion
-export { pathToFileURL };
